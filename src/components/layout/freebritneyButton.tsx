5c4e7c7cc7692a9ccf1a825d0fd26238b3110992
Tony Hermann <alejoh.developer@gmail.com>
2022-08-30 23:49:42 -0300

    .

--- src/components/layout/freebritneyButton.tsx
+++ src/components/layout/freebritneyButton.tsx
@@ -15,7 +15,7 @@ const FreeBritneyButton: FunctionComponent<FreeBritneyButtonProps> = () => {
   const [finish, setFinish] = useState<boolean>(false);
 
   const audioList = ["boys no se q cosa", "Do you feel me now", "dont stop now just be the champion", "don't u know i still believe", "Give me a sign", "hit me baby one more time", "I can't wait", "I still believe still believe", "I Think i did it again", "I'm addicted to you dont u know that ur toxic", "Im not that innocent", "Ima slave", "Intoxicate me now", "lollipop", "My lonliness is killing me", "now they dont believe ya but they gonna need ya", "oh baby baby", "oops 1", "oops2", "show me how u wanna to be", "There's no scape", "Too high can't come down", "u say im crazy i got u crazy", "U wanna hot", "u wanta  bugatti", "With the taste of your lips im on a ride", "womanizer 1", "work it out", "work work work", "you better work bitch", "you got me go in", "you wanna live fancy", "You wanna", "you want a lamborghini", "You want a maseratti"];
-  let [audioName, setAudioName] = useState<string>("");
+  let [audioName, setAudioName] = useState<string>("hit me baby one more time");
   let audio = new Audio(`./audio/${audioName}.mp3`);
 
 
